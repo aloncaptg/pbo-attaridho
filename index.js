1bf3f5a73b89b0128146f4682a7d98f009bad0d1
aloncaptg <73378170+aloncaptg@users.noreply.github.com>
2025-07-28 15:09:31 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,57 @@
+function sapa(nama){
+    console.log("Halo, " + nama + "!");
+
+}
+sapa("Joy")
+sapa("Seulgi")
+
+function tambah(a,b){
+    return a + b;
+
+}
+let hasil = tambah(5,3)
+    console.log("Hasil: " + hasil)
+
+    //
+    let car = {
+        name: "Fiat",
+        model: 500,
+        weight: "850kg",
+        color: "white",
+        start: function(){
+            console.log("Mobil dinyalakan")
+        },
+                drive: function(){
+            console.log("Mobil berjalan")
+        },
+                brake: function(){
+            console.log("Mobil Mengerem")
+        },
+                stop: function(){
+            console.log("Mobil Berhenti")
+        },
+    };
+car.start()
+car.drive(
+
+)
+
+    //
+ 
+    let bike = {
+        name: "Polygon",
+        Gear: 24,
+        weight: "25Kg",
+        color: "white",
+            drive: function(){
+            console.log("Mobil berjalan")
+        },
+                brake: function(){
+            console.log("Mobil Mengerem")
+        },
+    }
+    bike.drive()
+    bike.brake()
+    console.log(bike.name)
+        
+
